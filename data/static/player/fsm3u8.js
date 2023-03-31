3f84a782a55e717503251b2622511cede370e899
chousann <45139336+chousann@users.noreply.github.com>
2023-03-31 18:02:02 +0800

    Delete data directory

--- data/static/player/fsm3u8.js
+++
@@ -1,2 +0,0 @@
-MacPlayer.Html = '<iframe src="https://www.feisuplayer.com/m3u8/?url='+MacPlayer.PlayUrl+'" height="'+MacPlayer.Height+'" width="100%" scrolling="no" id="Player" allowfullscreen="true" ></iframe>';
-MacPlayer.Show();
