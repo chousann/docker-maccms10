b1285fbca906b8278b9b886b0e8557c29c033af0
chousann <1769862201@qq.com>
2023-05-20 00:23:48 +0900

    up

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,9 +1,3 @@
-sudo apt update
-sudo apt install curl
-#sudo apt install git
-#git clone https://github.com/chousann/docker-maccms10.git
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-cd docker-maccms10
+#!/bin/bash
 docker compose up -d
           
---
+++ .github/workflows/prepare.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt update
+if [[ ! "$(command -v git)"]];then
+  sudo apt -y install git
+fi
+
+if [[ ! "$(command -v curl)"]];then
+  sudo apt -y install curl
+fi
+
+if [[ ! "$(command -v docker)"]];then
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sh get-docker.sh
+fi
+
+if [[ -n $(docker ps -q  -f "name=^maccms$")]];then
+  if [ ! -d "docker-maccms10"];then
+  else
+    cd docker-maccms10
+    docker compose stop
+    docker compose rm
+    cd ../
+    rm -r ocker-maccms10
+  fi
+fi
+          
