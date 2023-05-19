06176a2287f9dbd4c7a3a37fe99ff5f6ea33331c
chousann <1769862201@qq.com>
2023-05-20 01:15:37 +0900

    up

--- .github/workflows/prepare.sh
+++ .github/workflows/prepare.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 sudo apt update
-if [[ ! "$(command -v git)"]];then
+if [[ ! "$(command -v git)"]]; then
   sudo apt -y install git
 fi
 
-if [[ ! "$(command -v curl)"]];then
+if [[ ! "$(command -v curl)"]]; then
   sudo apt -y install curl
 fi
 
-if [[ ! "$(command -v docker)"]];then
+if [[ ! "$(command -v docker)"]]; then
   curl -fsSL https://get.docker.com -o get-docker.sh
   sh get-docker.sh
 fi
 
-if [[ -n $(docker ps -q  -f "name=^maccms$")]];then
-  if [ ! -d "docker-maccms10"];then
+if [[ -n $(docker ps -q  -f "name=^maccms$")]]; then
+  if [ ! -d "docker-maccms10"]; then
   else
     cd docker-maccms10
     docker compose stop
