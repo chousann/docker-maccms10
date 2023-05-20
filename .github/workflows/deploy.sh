67220db66e88ac7308410c12be08317361c9040f
chousann <45139336+chousann@users.noreply.github.com>
2023-05-20 14:27:14 +0800

    Update deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+# 定义要停止的端口号
+port=80
+
+# 获取占用端口的进程ID
+pid=$(sudo lsof -t -i:${port})
+
+# 如果找到了进程ID，杀死该进程
+if [ -n "$pid" ]; then
+  sudo kill $pid
+  echo "进程 $pid 已被停止..."
+else
+  echo "没有找到占用端口 $port 的进程。"
+fi
 docker compose up -d
           
