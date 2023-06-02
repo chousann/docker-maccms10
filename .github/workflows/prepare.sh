b89df5d432b65e52393006c95b37126e7a5f77fb
chousann <45139336+chousann@users.noreply.github.com>
2023-06-02 19:59:58 +0800

    Update prepare.sh

--- .github/workflows/prepare.sh
+++ .github/workflows/prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /run/root
+cd /root
 sudo apt update
 #if [ ! "$(command -v git)" ]; then
 #  sudo apt -y install git
@@ -16,13 +16,13 @@ fi
 if [ ! "$(command -v docker)" ]; then
   curl -fsSL https://get.docker.com -o get-docker.sh
   sh get-docker.sh
-  mkdir -p /etc/docker
-  mkdir -p /run/root/docker
-  touch /etc/docker/daemon.json
-  echo -e "{" > /etc/docker/daemon.json
-  echo -e "\t\"data-root\":\"/run/root/docker\"" >> /etc/docker/daemon.json
-  echo -e "}" >> /etc/docker/daemon.json
-  systemctl restart docker
+  #mkdir -p /etc/docker
+  #mkdir -p /run/root/docker
+  #touch /etc/docker/daemon.json
+  #echo -e "{" > /etc/docker/daemon.json
+  #echo -e "\t\"data-root\":\"/run/root/docker\"" >> /etc/docker/daemon.json
+  #echo -e "}" >> /etc/docker/daemon.json
+  #systemctl restart docker
 fi
 
 if [ -n $(docker ps -q  -f "name=^maccms$") ]; then
